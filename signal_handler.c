022cd1ab8c86a4e9a2bb527dc7c1e5b1104db880
Ahmet Yasin Akyüz <aakyuz@k1m30s01.42istanbul.com.tr>
2025-03-03 09:15:46 +0300

    signal comments added

--- signal_handler.c
+++ signal_handler.c
@@ -6,33 +6,50 @@
 /*   By: aakyuz <aakyuz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 10:40:00 by aakyuz            #+#    #+#             */
-/*   Updated: 2025/03/02 15:05:26 by aakyuz           ###   ########.fr       */
+/*   Updated: 2025/03/03 09:10:37 by aakyuz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+//Derleyiciye, g_received_signal değişkeninin değerinin beklenmedik zamanlarda değişebileceğini bildirir.
 volatile sig_atomic_t	g_received_signal = 0;
 
 void	handle_sigint(int signum)
 {
+	//ctrl + c sinyali geldiğinde g_received_signal değişkenine sinyalin numarası atanır
 	g_received_signal = signum;
+	//ctrl + c sinyali geldiğinde terminalde yeni bir satır açılır
 	write(STDOUT_FILENO, "\n", 1);
+	
+	//Yeni bir satır başlatıldığını readline (rl) kütüphanesine bildiriyor.
 	rl_on_new_line();
+	//readline girdisini temizler
 	rl_replace_line("", 0);
+	//readline girdisini yeniden ekrana yazdırıyor, yani terminalde temizlenmiş bir satır görüntülenmesini sağlıyor.
 	rl_redisplay();
 }
 
 void	setup_signals(void)
 {
+	//ctrl + c ve ctrl + \ sinyalleri için sigaction yapısı oluşturuldu
 	struct sigaction	sa_int;
 	struct sigaction	sa_quit;
 
+	//SIGINT sinyali için handle_sigint fonksiyonunu çağır
 	sa_int.sa_handler = handle_sigint;
+
+	//sa_int tarafından kullanılan sinyal maskesi temizleniyor.
+	//Böylece başka sinyallerin de engellenmeden çalışmasına izin veriliyor.
 	sigemptyset(&sa_int.sa_mask);
+
+	//özel flaler olmadığı için 0
 	sa_int.sa_flags = 0;
+
+	//SIGINT sinyali için sa_int yapılandırması işletim sistemine bildiriliyor.
 	sigaction(SIGINT, &sa_int, NULL);
 
+	//SIGQUIT sinyali tamamen yok sayılıyor (SIG_IGN ile)
 	sa_quit.sa_handler = SIG_IGN;
 	sigemptyset(&sa_quit.sa_mask);
 	sa_quit.sa_flags = 0;
