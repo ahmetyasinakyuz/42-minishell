e9b6667bdcf9ab499e7a80050ecee2f5ed0209c3
Ahmet Yasin Akyüz <akyuzahmetyasin@gmail.com>
2025-04-22 20:03:57 +0300

    first exec

--- execute/none_built_in.c
+++ execute/none_built_in.c
@@ -6,15 +6,58 @@
 /*   By: aakyuz <aakyuz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 14:27:12 by aycami            #+#    #+#             */
-/*   Updated: 2025/04/22 19:00:53 by aakyuz           ###   ########.fr       */
+/*   Updated: 2025/04/22 19:45:46 by aakyuz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <signal.h>
 
 void none_built_in(t_simple_cmds *cmd_list, char **envp)
 {
 	char	*path;
-	path = path_finder(cmd_list->str[0] , envp);
-	printf("PATH:%s \n", path);
+	path = path_finder(cmd_list->content[0] , envp);
+	if (path == NULL)
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(cmd_list->content[0], 2);
+		ft_putstr_fd(": command not found\n", 2);
+		cmd_list->return_value = 127;
+		return ;
+	}
+	pid_t pid = fork();
+	if (pid == 0)
+	{
+		execve(path, cmd_list->content, envp);
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		int status;
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			cmd_list->return_value = WEXITSTATUS(status);
+		else if (WIFSIGNALED(status))
+			cmd_list->return_value = 126 + WTERMSIG(status);
+	}
 }
