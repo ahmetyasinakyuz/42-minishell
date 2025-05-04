c5c32db9416204e96163c1ac1b8b91bfe0a485e9
ahmetyasinakyuz <akyuzahmetyasin@gmail.com>
2025-05-04 12:40:37 +0300

    retun value

--- execute/builtin_control.c
+++ execute/builtin_control.c
@@ -6,7 +6,7 @@
 /*   By: aakyuz <aakyuz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 14:27:12 by aycami            #+#    #+#             */
-/*   Updated: 2025/05/04 12:28:40 by aakyuz           ###   ########.fr       */
+/*   Updated: 2025/05/04 12:39:55 by aakyuz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,38 @@
 
 void	builtin_control(t_simple_cmds *cmd_list, t_free *free_struct)
 {
-	// If no command or first element is empty string but there are more elements,
-	// shift everything left
 	if (!cmd_list->str || !cmd_list->str[0] || cmd_list->str[0][0] == '\0')
 	{
 		int i = 0;
-		if (cmd_list->str && cmd_list->str[0] && cmd_list->str[1])
+		if (cmd_list->str && cmd_list->str[1])
 		{
-			// Shift all strings to the left if first is empty
 			while (cmd_list->str[i + 1] != NULL)
 			{
 				cmd_list->str[i] = cmd_list->str[i + 1];
 				i++;
 			}
 			cmd_list->str[i] = NULL;
+			if (cmd_list->content && cmd_list->content[1])
+			{
+				i = 0;
+				while (cmd_list->content[i + 1] != NULL)
+				{
+					cmd_list->content[i] = cmd_list->content[i + 1];
+					i++;
+				}
+				cmd_list->content[i] = NULL;
+			}
 		}
-		
-		// If after shifting we have a command, execute it
 		if (cmd_list->str && cmd_list->str[0] && cmd_list->str[0][0] != '\0')
 			builtin_control(cmd_list, free_struct);
 		else
 			exit(0);
 		return;
 	}
-	
-	// Check for exit command
 	if (ft_strncmp("exit", cmd_list->str[0], 5) == 0)
 	{
 		free_struct->cmd_list = cmd_list;
 		exit_builtin(cmd_list, free_struct);
-		// If we get here, exit was called with too many arguments
 		exit(cmd_list->return_value);
 	}
 	else if (ft_strncmp("echo", cmd_list->str[0], 5) == 0)
--- execute/none_built_in.c
+++ execute/none_built_in.c
@@ -6,7 +6,7 @@
 /*   By: aakyuz <aakyuz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 14:27:12 by aycami            #+#    #+#             */
-/*   Updated: 2025/05/04 12:29:17 by aakyuz           ###   ########.fr       */
+/*   Updated: 2025/05/04 12:40:17 by aakyuz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ void	none_built_in_path_control(t_simple_cmds *cmd_list,
 	if (cmd_list->str[0][0] == '/' || (cmd_list->str[0][0] == '.'
 			&& cmd_list->str[0][1] == '/') || (cmd_list->str[0][0] == '.'
 			&& cmd_list->str[0][1] == '.' && cmd_list->str[0][2] == '/'))
-		*path = cmd_list->str[0];
+		*path = ft_strdup(cmd_list->str[0]);  // Use ft_strdup to avoid modifying original
 	else
 		*path = path_finder(cmd_list->str[0], envp);
 }
@@ -87,7 +87,16 @@ void	none_built_in(t_simple_cmds *cmd_list, char **envp)
 			ft_putstr_fd(": command not found\n", 2);
 			cmd_list->return_value = 127;
 		}
-		return ;
+		exit(cmd_list->return_value);
+	}
+	if (access(path, F_OK) != 0)
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(path, 2);
+		ft_putstr_fd(": No such file or directory\n", 2);
+		cmd_list->return_value = 127;
+		free(path);
+		exit(cmd_list->return_value);
 	}
 	if (stat(path, &path_stat) == 0 && S_ISDIR(path_stat.st_mode))
 	{
@@ -95,7 +104,17 @@ void	none_built_in(t_simple_cmds *cmd_list, char **envp)
 		ft_putstr_fd(path, 2);
 		ft_putstr_fd(": Is a directory\n", 2);
 		cmd_list->return_value = 126;
-		return;
+		free(path);
+		exit(cmd_list->return_value);
+	}
+	if (access(path, X_OK) != 0)
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(path, 2);
+		ft_putstr_fd(": Permission denied\n", 2);
+		cmd_list->return_value = 126;
+		free(path);
+		exit(cmd_list->return_value);
 	}
 	if (cmd_list->flag == NULL)
 		cmd = cmd_list->str;
@@ -104,5 +123,6 @@ void	none_built_in(t_simple_cmds *cmd_list, char **envp)
 	setup_child_signals();
 	execve(path, cmd, envp);
 	perror("execve");
+	free(path);
 	exit(EXIT_FAILURE);
 }
