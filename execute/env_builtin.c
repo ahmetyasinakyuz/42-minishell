41f5a0b1614b4041493601eff70457dd50205ba1
aysesude <camiaysesude@gmail.com>
2025-05-04 21:38:19 +0300

    env norm fix

--- execute/env_builtin.c
+++ execute/env_builtin.c
@@ -3,14 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   env_builtin.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aakyuz <aakyuz@student.42.fr>              +#+  +:+       +#+        */
+/*   By: aycami <aycami@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 14:45:55 by codespace         #+#    #+#             */
-/*   Updated: 2025/05/04 17:46:48 by aakyuz           ###   ########.fr       */
+/*   Updated: 2025/05/04 21:38:03 by aycami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
 
 #include "../minishell.h"
 
@@ -37,7 +35,7 @@ void	env_builtin(t_simple_cmds *cmd_list, char ***envp)
 	{
 		printf("This command only works without the flag.\n");
 		cmd_list->return_value = 1;
-		return;
+		return ;
 	}
 	i = 0;
 	while ((*envp)[i])
