3dfa8a8dd002f12e9841566642b01843ba1cf8df
ahmetyasinakyuz <akyuzahmetyasin@gmail.com>
2025-05-11 13:16:23 +0300

    read error fix

--- execute/echo_builtin.c
+++ execute/echo_builtin.c
@@ -6,11 +6,21 @@
 /*   By: aakyuz <aakyuz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 15:59:20 by aycami            #+#    #+#             */
-/*   Updated: 2025/05/04 20:03:43 by aakyuz           ###   ########.fr       */
+/*   Updated: 2025/05/11 13:15:31 by aakyuz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+static int	is_n_flag(char *str)
+{
+	if (!str)
+		return (0);
+	if (str[0] == '-' && str[1] == 'n' && str[2] == '\0')
+		return (1);
+		
+	return (0);
+}
 
 void	echo_builtin(t_simple_cmds *cmd_list)
 {
@@ -19,11 +29,13 @@ void	echo_builtin(t_simple_cmds *cmd_list)
 
 	i = 1;
 	flag = 0;
-	if ((cmd_list->content && cmd_list->content[1]) && !cmd_list->content[1][2]
-		&& (cmd_list->content[1][0]) == '-' && (cmd_list->content[1][1] == 'n'))
+	if (cmd_list->content && cmd_list->content[1])
 	{
-		flag = 1;
-		i = 2;
+		if (is_n_flag(cmd_list->content[1]))
+		{
+			flag = 1;
+			i = 2;
+		}
 	}
 	if (cmd_list->content)
 	{
